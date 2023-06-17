7662f2eb51dda884008cf514603c0c20789b07f6
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-06-17 20:50:45 +0200

    Add board function to give random cords

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,18 @@
 export default function Board() {
-    let arr = Array(10)
+    const SIZE = 10;
+
+    let arr = Array(SIZE)
         .fill(0)
-        .map(() => Array(10).fill(0));
+        .map(() => Array(SIZE).fill(0));
+
+    function getRandomCoordinates() {
+        return [Math.floor(Math.random() * SIZE), Math.floor(Math.random() * SIZE)];
+    }
 
     return {
         show: () => arr,
         isPopulated: (x, y) => Boolean(arr[x][y]),
         populate: (x, y) => (arr[x][y] = 1),
+        getRandomCoordinates,
     };
 }
