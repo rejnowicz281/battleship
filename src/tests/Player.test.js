40a18e43be95add273fc5291688c91480400f8f7
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-06-18 21:06:50 +0200

    Add tests for player

--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -1,10 +1,30 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import Player from "../components/Player.js";
+import Ship from "../components/Ship.js";
 
 describe("Player", () => {
-    const player = Player();
+    let player;
 
-    it("Happy path", () => {
-        expect(player).toBeTruthy();
+    beforeEach(() => {
+        player = Player();
+    });
+
+    it("Adds ship", () => {
+        player.addShip(Ship(2, [0, 0]));
+    });
+
+    it("Returns false if adding illegal ship", () => {
+        player.addShip(Ship(2, [0, 0]));
+        expect(player.addShip(Ship(2, [0, 0]))).toBe(false);
+    });
+
+    it("Detects if all ships are destroyed", () => {
+        player.addShip(Ship(2, [0, 0])); // [0, 0], [0, 1]
+        player.addShip(Ship(1, [1, 0])); // [1, 0]
+        player.getShips()[0].hit();
+        player.getShips()[0].hit();
+        expect(player.allShipsDestroyed()).toBe(false);
+        player.getShips()[1].hit();
+        expect(player.allShipsDestroyed()).toBe(true);
     });
 });
